2735b05f746290efb533e1f0122c76c34fde0c05
IsaquePemasi <ADSisaque@gmail.com>
2023-05-14 15:39:05 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetAmigoMVC.Models;
+using System.Diagnostics;
+
+namespace PetAmigoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Adocao()
+        {
+            return View();
+        }
+
+        public IActionResult Doacao()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
