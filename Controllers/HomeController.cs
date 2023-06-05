ed748e51851cb1ed58ea84e3bbe2861d9087a07d
IsaquePemasi <ADSisaque@gmail.com>
2023-06-05 20:50:55 -0300

    add

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace PetAmigoMVC.Controllers
             return View();
         }
 
+        public IActionResult Perdido()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
