0bb499e0b4cb130c58a4bce6c02b69246987ff2e
IsaquePemasi <ADSisaque@gmail.com>
2023-06-07 20:48:32 -0300

    add

---
+++ Controllers/PerdidosController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetAmigoMVC.Controllers
+{
+    public class PerdidosController : Controller
+    {
+        public IActionResult Bidu()
+        {
+            return View();
+        }
+        public IActionResult BiruleiNina()
+        {
+            return View();
+        }
+        public IActionResult Brisa()
+        {
+            return View();
+        }
+        public IActionResult Mel()
+        {
+            return View();
+        }
+        public IActionResult Peteco()
+        {
+            return View();
+        }
+        public IActionResult PitocoChris()
+        {
+            return View();
+        }
+        public IActionResult Princesa()
+        {
+            return View();
+        }
+        public IActionResult Spike()
+        {
+            return View();
+        }
+        public IActionResult Zeka()
+        {
+            return View();
+        }
+    }
+}
