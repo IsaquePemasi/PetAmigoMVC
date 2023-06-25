e59c5ebde7aa9672e49e76fe65acfaa73e0cf51d
IsaquePemasi <ADSisaque@gmail.com>
2023-06-25 18:01:39 -0300

    ADD petshop #56

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,6 +32,10 @@ namespace PetAmigoMVC.Controllers
         {
             return View();
         }
+        public IActionResult Produtos()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
