81725282008f14b8040b0c02a5b8e9f38e7a0a8a
IsaquePemasi <ADSisaque@gmail.com>
2023-06-27 14:48:10 -0300

    Colocar uma página de parceiros e colocar a SP INVISÍVEL #60

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,7 +36,10 @@ namespace PetAmigoMVC.Controllers
         {
             return View();
         }
-
+        public IActionResult Parceiros()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
